cb5d188bfd389c36de1321d456c53730d0a7e88c
Choi Seok Won <55749752+wjsxkxk1@users.noreply.github.com>
2020-05-29 14:47:41 +0900

    h

--- templates/run.py
+++ templates/run.py
@@ -59,4 +59,4 @@ def training():
 	return jsonify(resultdata=resultdata)
 	
 if __name__ == "__main__":
-    app.run() #host='127.0.0.1',port='8888'
+    app.run(host='0.0.0.0',port='8000') #host='127.0.0.1',port='8888'
